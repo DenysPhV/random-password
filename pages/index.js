02269f09e2ec8918651dbdce2ed939997e935417
Denys Filichkin <denys.philichkin@gmail.com>
2021-10-14 20:40:04 +0300

    a work at styles

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css';
 
 const index = () => {
   const LENGTH_PASSWORD = [
+    { id: 0, name: 'Select length', value: 0 },
     { id: 1, name: 'Simple', value: 8 },
     { id: 2, name: 'Middle', value: 12 },
     { id: 3, name: 'Complicated', value: 15 },
@@ -31,64 +32,65 @@ const index = () => {
     setLength(e.target.value);
   };
 
-  function handleClick(e) {
-    console.log('Кликнул', password);
-
+  const handleClick = e => {
+    console.log('Получить пароль', password);
     window.navigator.clipboard.writeText(password);
-
     setPassword('Copied!');
-  }
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Generate a random password</h1>
+
       <form className={styles.form}>
-        {/* Переписать на селект с выбором длины пароля */}
         <div className={styles.selectContainer}>
-          <select onChange={handleUpdate}>
+          <select onChange={handleUpdate} className={styles.select}>
             {[...LENGTH_PASSWORD].map(item => {
               const { id, name, value } = item;
               return (
-                <option key={id} value={value}>
+                <option key={id} value={value} className={styles.options}>
                   {name}: {value}
                 </option>
               );
             })}
           </select>
         </div>
-        <h2>Note: Select the required checkbox! </h2>
-        <ul className={styles.checkboxItem}>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                checked={isLowerCase}
-                onChange={() => setIsLowerCase(value => !value)}
-              />
-              <span>LowerCase</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                checked={isUpperCase}
-                onChange={() => setIsUpperCase(value => !value)}
-              />
-              <span>UpperCase</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input
-                type="checkbox"
-                checked={isNumbers}
-                onChange={() => setIsNumbers(value => !value)}
-              />
-              <span>Numbers</span>
-            </label>
-          </li>
-        </ul>
+
+        <div>
+          <h2>Note: Select the required checkbox! </h2>
+          <ul className={styles.checkboxItem}>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isLowerCase}
+                  onChange={() => setIsLowerCase(value => !value)}
+                />
+                <span>LowerCase</span>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isUpperCase}
+                  onChange={() => setIsUpperCase(value => !value)}
+                />
+                <span>UpperCase</span>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isNumbers}
+                  onChange={() => setIsNumbers(value => !value)}
+                />
+                <span>Numbers</span>
+              </label>
+            </li>
+          </ul>
+        </div>
 
         <div>
           <button
